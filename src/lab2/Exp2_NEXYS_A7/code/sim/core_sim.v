4459ab00245712a3b0a26dcca232a22d72975465
Yusux <syusux@gmail.com>
2023-11-12 17:00:18 +0800

    feat: finish lab2

--- src/lab2/Exp2_NEXYS_A7/code/sim/core_sim.v
+++ src/lab2/Exp2_NEXYS_A7/code/sim/core_sim.v
@@ -3,6 +3,8 @@
 module core_sim;
     reg clk, rst;
 
+    reg interrupter;
+
     RV32core core(
         .debug_en(1'b0),
         .debug_step(1'b0),
@@ -10,13 +12,18 @@ module core_sim;
         .debug_data(),
         .clk(clk),
         .rst(rst),
-        .interrupter(1'b0)
+        .interrupter(interrupter)
     );
 
     initial begin
         clk = 0;
         rst = 1;
+        interrupter = 0;
         #2 rst = 0;
+        #30 interrupter = 1;
+        #2 interrupter = 0;
+        #38 interrupter = 1;
+        #2 interrupter = 0;
     end
     always #1 clk = ~clk;
 
